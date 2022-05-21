623fb59bc91b44b417f8e0e5f66127b3ed95e2ec
Katsushun89 <Katsushun89@gmail.com>
2022-05-21 21:23:23 +0900

    Add config.h, cert.h.

---
+++ src/cert.h
@@ -0,0 +1,13 @@
+#pragma once
+
+const char *root_ca = R"(-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+)";
+
+const char *certificate = R"(-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+)";
+
+const char *private_key = R"(-----BEGIN RSA PRIVATE KEY-----
+-----END RSA PRIVATE KEY-----
+)";
---
+++ src/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//WiFi
+const char* SSID = "xxxxxxxxxx";
+const char* PASS = "xxxxxxxx";
+
+//AWS IOT
+const char* AWS_IOT_ENDPOINT = "xxxxxxxxx";
+const uint32_t AWS_IOT_PORT = 8883;
+const char* THING_NAME = "xxx_sensor";
+const char* TOPIC = "sensor/device_name/sensor_update";
+
+
+
